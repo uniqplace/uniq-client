023e88fa95586277585fac4c0bf87b1d097385d1
mby123!@# <mbycastory@gmail.com>
2025-07-21 21:02:01 +0300

    delete Token

--- src/api/apiSlice.ts
+++ src/api/apiSlice.ts
@@ -1,4 +1,5 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import type { RootState } from '../store';
 
 const apiSlice = createApi({
   reducerPath: 'api',
@@ -6,12 +7,11 @@ const apiSlice = createApi({
     baseUrl: 'http://localhost:5002',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
-     // const state = getState() as Partial<RootState>;
-      //const token = state?.auth?.token;
-      const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY4NzhmNjIyYTY1ZjlkYTYwMmU4NDdjMCIsImlhdCI6MTc1Mjc1ODM3MCwiZXhwIjoxNzUzMzYzMTcwfQ.Bm9JBhsuTfx3PPStvfQaIARgtUVdOpuu_xW89zAqe0g";
-      // if (!state?.auth) {
-      //   console.warn('Warning: auth slice is missing from the root state!');
-      // }
+     const state = getState() as Partial<RootState>;
+      const token = state?.auth?.token;
+      if (!state?.auth) {
+        console.warn('Warning: auth slice is missing from the root state!');
+      }
       if (token) {
         headers.set('authorization', `Bearer ${token}`);
       }
