6dfc942205db84eb9bca7056ab80024df6f5a71c
RutShira <rutshi054853@gmail.com>
2025-07-28 16:58:25 +0300

    up

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -3,8 +3,9 @@ import { io } from 'socket.io-client';
 const socketUrl = import.meta.env.VITE_SOCKET_URL;
 const socket = io(socketUrl, {
   withCredentials: true,
-  transports: ['websocket']
-  
+  transports: ['websocket'],
+  path: import.meta.env.VITE_SOCKET_PATH || '/socket.io'
+
 });
 
 export default socket;
