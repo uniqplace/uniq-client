ebce3d9afb350e952c3ebcde6c57a1013a56966e
Chaya Flamm <144627530+chayaflam@users.noreply.github.com>
2025-07-29 17:57:08 +0300

    Update api.ts
    try url server

--- src/services/api.ts
+++ src/services/api.ts
@@ -5,7 +5,7 @@
 import type { Category, Product } from '../types';
 import axios from 'axios';
 console.log("API_BASE_URL:", import.meta.env.VITE_API_BASE_URL);
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://uniq-backend-o4mp.onrender.com/api';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 console.log("import.meta.env:", import.meta.env);
 console.log("VITE_API_BASE_URL:", import.meta.env.VITE_API_BASE_URL);
 
