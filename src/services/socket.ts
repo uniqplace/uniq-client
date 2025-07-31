a385c957bb4b05035846227bf4e33540c776ebe5
Mini <154057785+MiniDzaldov@users.noreply.github.com>
2025-07-31 12:07:00 +0300

    Update socket.ts

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -1,11 +1,11 @@
 import { io, Socket } from 'socket.io-client';
 
 const socketUrl = import.meta.env.VITE_SOCKET_URL;
-# const socket = io(socketUrl, {
-#  withCredentials: true,
-#  transports: ['websocket'],
-#  path: import.meta.env.VITE_SOCKET_PATH || '/api/socket.io'
-# });
+// const socket = io(socketUrl, {
+//  withCredentials: true,
+//  transports: ['websocket'],
+//  path: import.meta.env.VITE_SOCKET_PATH || '/api/socket.io'
+// });
 
 
 export const initializeSocket = (): Socket => {
