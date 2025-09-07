6c51f12b930d9ed66eb2be942ad660aeeb487ee7
Tamar Bollak <t0583230077@gmail.com>
2025-09-07 11:42:24 +0300

    Update steps.tsx

--- src/features/deployProcess/components/Stepper/steps.tsx
+++ src/features/deployProcess/components/Stepper/steps.tsx
@@ -1,4 +1,4 @@
-// import React, { createRef } from 'react};
+// import React, { createRef } from 'react';
 import React, { useEffect } from 'react';
 import FinishStepButton from './finishStepButton';
 import ManufacturerPreferencesStep from '../ManufacturerPreferencesStep';
