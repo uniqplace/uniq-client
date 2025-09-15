ab6aa06674cd3cb1d36e8e6c0e6f2c97967d6eb9
mby123!@# <mbycastory@gmail.com>
2025-09-15 10:07:48 +0300

    update defaultProductTemplate due to TS errors

--- src/utils/defaultProductTemplate.ts
+++ src/utils/defaultProductTemplate.ts
@@ -3,10 +3,13 @@ export const defaultProductTemplate = {
   description: 'This is a hardcoded product',
   price: 123,
   images: [],
-  category: 'hardcoded-category',
+  category: {
+    _id: "64a7f0c8f1f1e2b4c8d0e9a1",
+    name: "Hard category "
+  },
   subCategories: [],
-  condition: 'new',
+  condition: 'new' as 'new' | 'like_new' | 'good' | 'fair' | 'poor',
   location: 'Hardcoded Location',
   tags: ['hardcoded'],
-  status:'published'
+  status: "published" as "published" | "draft" | "hidden",
 };
